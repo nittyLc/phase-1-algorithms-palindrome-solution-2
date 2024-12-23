762166fa95f8c85f93e8eed02aeb204a56e13821
caleb <musyokacaleb5@gmail.com>
2024-12-23 10:12:59 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
+// function isPalindrome(word) {
+//   // Write your algorithm here
+// }
+
+// /* 
+//   Add your pseudocode here
+// */
+
+// /*
+//   Add written explanation of your solution here
+// */
+
+// // You can run `node index.js` to view these console logs
+// if (require.main === module) {
+//   // add your own custom tests in here
+//   console.log("Expecting: true");
+//   console.log("=>", isPalindrome("racecar"));
+
+//   console.log("");
+
+//   console.log("Expecting: false");
+//   console.log("=>", isPalindrome("robot"));
+// }
+
+// module.exports = isPalindrome;
+
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+  // Convert the word to lowercase (optional, if case sensitivity isn't required)
+  // const lowercasedWord = word.toLowerCase();
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+  // Reverse the word and check if it matches the original
+  const reversedWord = word.split("").reverse().join("");
+  return word === reversedWord;
 }
 
 module.exports = isPalindrome;
